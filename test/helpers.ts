65d02834537bf3b76e20e7829cf7e71732e58d19
Muhammad Ali Mazhar butt <57632737+MUHAMMAD-ALI-MAZHAR-BUTT@users.noreply.github.com>
2025-04-25 03:45:29 +0500

    Test/load testing (#1)
    * Add package lock file
    
    * add package json
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update file
    
    * update load
    
    * feat: artifacts add
    
    * update artifact version
    
    * update for s3
    
    * remove html report generation
    
    ---------
    
    Co-authored-by: Sam Austin <sam@ordinalsbot.com>

--- test/helpers.ts
+++
@@ -1,5 +0,0 @@
-
-export const toSupabaseResponse = (data: any, error: any = null) => {
-    const count = Array.isArray(data) ? data.length : 1;
-    return { data, error, count, status: 200, statusText: 'OK' };
-};
